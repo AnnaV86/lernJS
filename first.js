f90bb30bc7e6b0dfb2dfb7919f557295b6fb31e8
AnnaV86 <a.vidutina@yandex.ru>
2021-08-24 22:25:03 +0500

    3 day

--- first.js
+++ first.js
@@ -1,2 +1,10 @@
-console.log('Hello world')
-console.log('Hello world2')
+// BEGIN (write your solution here)
+const getHiddenCard = (num, star = 4) => {
+    const sliceNum = num.slice(-4);
+    const padStar = sliceNum.padStart(star + 4, '*');
+    return console.log(padStar);
+
+};
+getHiddenCard('1234567812345678');
+
+  // END
