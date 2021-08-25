14d806b5572ce83c5a55a7fc174dbb524293eb87
AnnaV86 <a.vidutina@yandex.ru>
2021-08-25 12:18:41 +0500

    4 day

--- first.js
+++ first.js
@@ -1,10 +1,10 @@
 // BEGIN (write your solution here)
-const getHiddenCard = (num, star = 4) => {
-    const sliceNum = num.slice(-4);
-    const padStar = sliceNum.padStart(star + 4, '*');
-    return console.log(padStar);
+const isLannisterSoldier = (color, shild) => {
+  const lannister = (color === 'red' && shild === 'null') || shild === 'lion';
+  return lannister;
+}
 
-};
-getHiddenCard('1234567812345678');
 
-  // END
+console.log(isLannisterSoldier('red', 'lion'));
+console.log(isLannisterSoldier('blue', null));
+// END
