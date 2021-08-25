56da317cb50766d54e52e67db97c1e6953d28883
AnnaV86 <a.vidutina@yandex.ru>
2021-08-26 01:36:24 +0500

    4 day

--- first.js
+++ first.js
@@ -1,10 +1,6 @@
-// BEGIN (write your solution here)
-const isLannisterSoldier = (color, shild) => {
-  const lannister = (color === 'red' && shild === 'null') || shild === 'lion';
-  return lannister;
-}
-
-
-console.log(isLannisterSoldier('red', 'lion'));
-console.log(isLannisterSoldier('blue', null));
-// END
+const getSentenceTone = (sentence) => {
+  if (sentence.toUpperCase() === sentence) {
+    return 'scream';
+  }
+  return 'general';
+};
