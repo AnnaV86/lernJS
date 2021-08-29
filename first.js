9de16719fc00e96d44850d0e25f260d3e5f7df5d
AnnaV86 <a.vidutina@yandex.ru>
2021-08-29 18:56:08 +0500

    7 day

--- first.js
+++ first.js
@@ -1,5 +1,7 @@
-const buildUrl = (pageAddress, domain) => `https://${domain + pageAddress.replace(/^\/|()/, '/$1')}`
+const buildUrl = (pageAddress, domain) => `https://${domain + pageAddress
+    .replace(/.*(?=pages)/, '')
+    .replace(/.*/, x => '/' + x[0].toUpperCase() + x.slice(1).replace(/\/a/, '/A'))}`
 
 
-console.log(buildUrl('pages/about', 'hexlet.io'))
-console.log(buildUrl('/pages/about', 'hexlet.io'))
+console.log(buildUrl('ahftgx/pages/about', 'hexlet.io'))
+console.log(buildUrl('gfdgfdgdfgdfg/pages/about', 'hexlet.io'))
