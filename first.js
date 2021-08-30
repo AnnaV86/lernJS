f7c820ac1f2dc11aac2d4c8b2ec7e43f8008d9da
AnnaV86 <a.vidutina@yandex.ru>
2021-08-30 23:14:12 +0500

    task

--- first.js
+++ first.js
@@ -1,3 +1,15 @@
-function numberToPower(number, power) {
-    // Code here
+const filterString = (str, symbol) => {
+    let i = 0;
+    let result = ''
+    while (i < str.length) {
+        if (str[i] !== symbol) {
+            result = `${result}${str[i]}`
+        }
+        i += 1
+    }
+    return console.log(result)
 }
+
+const text = 'If I look back I am lost'
+filterString(text, 'I')
+filterString('zz zorro', 'z')
