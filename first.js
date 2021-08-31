e6233baeb491dd9400c169cd68338c5a9f24386c
AnnaV86 <a.vidutina@yandex.ru>
2021-08-31 18:29:39 +0500

    semilicon

--- first.js
+++ first.js
@@ -1,10 +1,10 @@
 const encrypt = (text) => {
-    let result = ''
+    let result = '';
     for (let i = 0; i < text.length; i += 2) {
         const nextSymbol = text[i + 1] || '';
         result = `${result}${nextSymbol}${text[i]}`
 
-    } return console.log(result)
+    } return console.log(result);
 }
 
 encrypt('attack'); // 'taatkc'
