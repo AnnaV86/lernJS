7fe598837906d4923eb8c5514b89497836b120ab
AnnaV86 <a.vidutina@yandex.ru>
2021-09-02 23:04:15 +0500

    learn new

--- learnjs.js
+++ learnjs.js
@@ -1,1 +1,11 @@
-alert('Я JavaScript!')
+let numbers = [2, 3, 4, 5, 10, 18, 23]
+
+function evenSum(arr) {
+    let summa = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (arr[i] % 2 === 0) {
+            summa = summa + arr[i]
+        }
+    } return summa
+}
+console.log(evenSum(numbers))
