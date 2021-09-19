cd893f47666f72e953fb56231ac799fcbbaf03f0
AnnaV86 <a.vidutina@yandex.ru>
2021-09-19 17:10:55 +0500

    19/09/2021

--- codewars.js
+++ codewars.js
@@ -472,7 +472,7 @@
 // console.log(paperwork(5, -5))
 
 function firstNonConsecutive(arr) {
-    if arr[]
-
+    const result = arr.find((el, i, m) => el - 1 !== m[i - 1] && i !== 0)
+    return typeof result === 'number' ? result : null
 }
-console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]))
