ebad4a7cb23a1d314c87cf10bafd0ec0601dda65
AnnaV86 <a.vidutina@yandex.ru>
2021-10-16 10:53:21 +0500

    16/10/2021

--- codewars.js
+++ codewars.js
@@ -1380,13 +1380,48 @@ function likes(names) {
 // console.log(number(["a", "b", "c"]))
 // console.log(number([]))
 
-function getDivisorsCnt(n) {
-    const arr = []
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            arr.push(i)
-        }
+// function getDivisorsCnt(n) {
+//     const arr = []
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i === 0) {
+//             arr.push(i)
+//         }
+//     }
+//     return arr.length
+// }
+// console.log(getDivisorsCnt(54))
+
+// function solution(digits) {
+//     return [...digits].reduce((acc, el, index, array) => {
+//         const newNumber = Number(array.slice(index, index + 5).join(''))
+//         return acc = newNumber > acc ? newNumber : acc
+//     }, 0)
+// }
+
+// console.log(solution('1234567898765'))
+// console.log(solution('7316717653133062491922511967442657474235534919493496983520368542506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753123457977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257540920752963450'))
+// console.log(solution('731674765'))
+
+// const sequenceSum = (begin, end, step) => {
+//     let acc = 0
+//     for (let i = begin; i <= end; i += step) {
+//         acc += i
+//     }
+//     return acc
+// }
+
+
+// console.log(sequenceSum(2, 6, 2))
+// console.log(sequenceSum(1, 5, 1))
+// console.log(sequenceSum(1, 5, 3))
+
+function arithmetic(a, b, operator) {
+    const operators = {
+        "add": a + b,
+        "subtract": a - b,
+        "multiply": a * b,
+        "divide": a / b
     }
-    return arr.length
+    return operators[operator]
 }
-console.log(getDivisorsCnt(54))
+console.log(arithmetic(8, 2, "subtract"))
