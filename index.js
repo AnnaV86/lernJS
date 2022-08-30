69ee7e04569d0375910c9f1166d6bb56dd9919c4
Anna Vidutina <a.vidutina@yandex.ru>
2022-08-30 08:42:56 +0500

    30082022

--- index.js
+++ index.js
@@ -52,14 +52,66 @@
 // console.log(removeRotten([]));
 // console.log(removeRotten(['apple', 'rottenBanana', 'apple']));
 
-function strong(n) {
-  const number = [...String(n)]
-    .map((el) => [...Array(Number(el))].map((el, ind) => (el = ind + 1)))
-    .map((el) => el.reduce((acc, el) => acc * el, 1))
-    .reduce((acc, el) => acc + el, 0);
-  return number === n ? 'STRONG!!!!' : 'Not Strong !!';
-}
+// function strong(n) {
+//   const number = [...String(n)]
+//     .map((el) => [...Array(Number(el))].map((el, ind) => (el = ind + 1)))
+//     .map((el) => el.reduce((acc, el) => acc * el, 1))
+//     .reduce((acc, el) => acc + el, 0);
+//   return number === n ? 'STRONG!!!!' : 'Not Strong !!';
+// }
 
-console.log(strong(2));
-console.log(strong(145));
-console.log(strong(7));
+// console.log(strong(2));
+// console.log(strong(145));
+// console.log(strong(7));
+
+// const spacer = (stringLength, textArr) => {
+//   const arrLength = textArr
+//     .map((el) => el.length)
+//     .reduce((acc, el) => acc + el);
+//   const textArrEl = textArr.length;
+
+//   if (textArrEl === 1) {
+//     const smallString = textArr
+//       .map((el) => `${el}${' '.repeat(stringLength - arrLength)}`)
+//       .join('');
+//     return smallString;
+//   }
+
+//   const spiceCount = Math.floor((stringLength - arrLength) / (textArrEl - 1));
+//   const stringResult = textArr
+//     .map((el, ind) => {
+//       if (stringLength - arrLength !== spiceCount * (textArrEl - 1)) {
+//         const addSpice =
+//           stringLength - arrLength - spiceCount * (textArrEl - 1);
+//         return ind < addSpice ? `${el} ` : el;
+//       } else return el;
+//     })
+//     .join(' '.repeat(spiceCount));
+
+//   return stringResult;
+// };
+
+// console.log(spacer(20, ['this', 'is', 'example']));
+// console.log(spacer(30, ['123456', '7890123', '456', '78']));
+// console.log(spacer(10, ['1', '23', '45']));
+// console.log(spacer(13, ['1', '23', '45', '678']));
+// console.log(spacer(8, ['12345']));
+
+// function growingPlant(upSpeed, downSpeed, desiredHeight, heigth = 0, day = 1) {
+//   const heightAfterDay = heigth + upSpeed;
+//   const heightAfterNigth = heightAfterDay - downSpeed;
+//   return heightAfterDay > desiredHeight - 1
+//     ? day
+//     : growingPlant(upSpeed, downSpeed, desiredHeight, heightAfterNigth, ++day);
+// }
+
+// // console.log(growingPlant(100, 10, 910));
+// console.log(growingPlant(10, 9, 4));
+
+// function repeats(arr) {
+//   return arr
+//     .filter((num) => arr.indexOf(num) === arr.lastIndexOf(num))
+//     .reduce((acc, num) => num + acc, 0);
+// }
+
+// console.log(repeats([4, 5, 7, 5, 4, 8]));
